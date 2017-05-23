de336dd68fda9c5d899e7871e0c2b505f54fec8f
Javier Moreno <javier.moreno.vilaplana@hotmail.com>
2017-05-23 16:44:44 +0200

    modified:   fotografos.html 	modified:   ordenes_database.sql

--- webdivs/ordenes_database.sql
+++ webdivs/ordenes_database.sql
@@ -1,20 +1,60 @@
-create database photoweb;
 
 create table usuarios(
 
 usuario varchar(20),
-contraseña number(10)
+contrasenya varchar(15) NOT NULL,
+CONSTRAINT PRIMARY KEY(usuario)
 );
 
 create table imagenes(
 
-id_imagen varchar(20),
-ruta varchar(50)
+id_imagen integer,
+ruta varchar(50) NOT NULL,
+CONSTRAINT imagenes_pk PRIMARY KEY(id_imagen)
 );
 
 create table fotografos(
 
 nombre varchar (25),
 biografia varchar(2000),
-quote varchar (150)
+quote varchar (150),
+CONSTRAINT fotografos_pk PRIMARY KEY(nombre)
 );
+
+insert into usuarios (usuario, contrasenya) 
+VALUES (JavierM, 1234);
+
+insert into imagenes(id_imagen, ruta)
+VALUES(9,'annie.jpg)';
+
+insert into imagenes(id_imagen, ruta)
+VALUES(2,'bresson.jpg');
+
+insert into imagenes(id_imagen, ruta)
+VALUES(1,'capa.jpg');
+
+insert into imagenes(id_imagen, ruta)
+VALUES(5,'carter.jpg');
+
+insert into imagenes(id_imagen, ruta)
+VALUES(4,'fass.jpg');
+
+insert into imagenes(id_imagen, ruta)
+VALUES(8,'madoz.jpg');
+
+insert into imagenes(id_imagen, ruta)
+VALUES(3,'manray.jpg');
+
+insert into imagenes(id_imagen, ruta)
+VALUES(6,'macurry.jpg');
+
+insert into imagenes(id_imagen, ruta)
+VALUES(10,'newton.jpg');
+
+insert into imagenes(id_imagen, ruta)
+VALUES(7,'sanz.jpg');
+
+
+
+
+
